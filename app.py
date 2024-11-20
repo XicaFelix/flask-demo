803c61bf9d2cda1ef98dcda583e977ffe3bd40e6
Xica Felix <xicasfelix@gmail.com>
2024-11-20 13:13:39 -0500

    create demo flask project

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/hello")
+def hello():
+  return 'Hello'
+
+@app.route("/user/<username>")
+def show_username(username):
+  return f"user {escape(username)}"
+
+@app.route("/about")
+def about():
+  return render_template("about.html")
